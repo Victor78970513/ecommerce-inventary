fc44fea564aa17343957f1aa0c29b69f07310bd1
Victor78970513 <yer29.chok@gmail.com>
2024-02-09 18:52:36 -0400

    refactored category => bloc

--- lib/presentation/products/bloc/categories/category_state.dart
+++ lib/presentation/products/bloc/categories/category_state.dart
@@ -6,29 +6,20 @@ class CategoryState {
   final bool loadingCategories;
   final bool error;
 
-  final int indexColumn1;
-  final int indexColumn2;
-
   const CategoryState({
     this.categories = const [],
     this.loadingCategories = true,
     this.error = false,
-    this.indexColumn1 = 0,
-    this.indexColumn2 = 0,
   });
 
   CategoryState copyWith({
     List<Category>? categories,
     bool? loadingCategories,
     bool? error,
-    int? indexColumn1,
-    int? indexColumn2,
   }) =>
       CategoryState(
         categories: categories ?? this.categories,
         loadingCategories: loadingCategories ?? this.loadingCategories,
         error: error ?? this.error,
-        indexColumn1: indexColumn1 ?? this.indexColumn1,
-        indexColumn2: indexColumn2 ?? this.indexColumn2,
       );
 }
